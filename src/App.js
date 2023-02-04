6283b40896df0a49cb3be3c8f5fc0f34975a83c2
maiyo9x <maiyonaisu1102@gmail.com>
2023-02-04 16:52:03 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <div>Đây là AppComponent</div>
+    </div>
+  );
+}
+
+export default App;
